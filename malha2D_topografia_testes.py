94a85c2448bebd9ebd2912eb7683970b380e59e6
albertoakel <alberto.akel@gmail.com>
2020-07-31 16:07:07 -0300

    malha_2d_topografia_teste

--- malha2D_topografia_testes.py
+++ malha2D_topografia_testes.py
@@ -10,6 +10,7 @@ from SimPEG import Mesh, Utils
 from discretize.utils import mkvc, refine_tree_xyz
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 
 
 style_list = ['default', 'classic'] + sorted(
@@ -62,7 +63,7 @@ M.x0 = np.r_[-(nbcx*dh)/2,-15000]
 
 
 #definir camada como função
-xx = np.linspace(-25600.0,25600.0,8*nbcx)
+xx = np.linspace(-25600.0,25600.0,10*nbcx)
 yy = 400*np.sin((2*xx*np.pi)/10000)
 #yy = 160*np.sin((2*xx*np.pi-3000)/100000)
 
@@ -73,16 +74,13 @@ M = refine_tree_xyz(M, pts, octree_levels=[2, 2], method='radial', finalize=Fals
     )
 
 
-
-
-
 M.finalize()  # Must finalize tree mesh before use
 
 s=np.zeros(M.nC) + 0
 
 k=len(M.gridCC)
 xx = np.linspace(-25600.0,25600.0,k)
-yy = 100*np.sin((2*xx*np.pi)/10000)
+yy =400*np.sin((2*xx*np.pi)/10000)
 
 for i in range(0,len(yy),1):
 #    print('y',[i,yy[i]])
@@ -93,29 +91,50 @@ for i in range(0,len(yy),1):
 
 
 
-fig = plt.figure(figsize=(6, 6))
-ax = fig.add_subplot(111)
+fig1 = plt.figure(figsize=(6, 6))
+fig1.canvas.set_window_title('Topografia grid 2D')
+
+ax = fig1.add_subplot(111)
 M.plotImage(s,grid=True, ax=ax)
-ax.set_title('teste')
+ax.set_title('Topografia - Teste octree 2D')
+
+
+
+
+
+
+
+
+
 
 #nl,nc,p
-#ax1 = plt.subplot(211)
-#M.plotImage(s, grid=True, ax=ax1)
-#ax1.set_title('a')
-##
-#ax2 = plt.subplot(223)
-#ax2.margins(2, 2)           # Values >0.0 zoom out
-#M.plotImage(s, grid=True, ax=ax2)
-#ax2.set_title('b')
-#plt.xlim(-1000,1000)
-#plt.ylim(-500,500)
-##
-#ax3 = plt.subplot(224)
-#ax3.margins(x=0, y=-0.25)   # Values in (-0.5, 0.0) zooms in to center
-#M.plotImage(s, grid=True, ax=ax3)
-#ax3.set_title('c')
-#plt.xlim(-300,300)
-#plt.ylim(-300,300)
+fig2 = plt.figure()
+fig2.canvas.set_window_title('Topografia grid 2D - Zoom')
+
+ax1 = fig2.add_subplot(221)
+M.plotImage(s, grid=True, ax=ax1)
+ax1.set_title('a')
+plt.xlim(-20000,-10000)
+plt.ylim(-2000,2000)
+#
+ax2 = fig2.add_subplot(222)
+M.plotImage(s, grid=True, ax=ax2)
+ax2.set_title('b')
+plt.xlim(-10000,0000)
+plt.ylim(-2000,2000)
+#
+ax3 = fig2.add_subplot(223)
+M.plotImage(s, grid=True, ax=ax3)
+ax3.set_title('c')
+plt.xlim(-20000,-10000)
+plt.ylim(-1000,1000)
+
+ax4 = fig2.add_subplot(224)
+M.plotImage(s, grid=True, ax=ax4)
+ax3.set_title('d')
+plt.xlim(-5000,0000)
+plt.ylim(-600,600)
+
 
 
 
